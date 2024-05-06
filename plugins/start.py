528a8eca7882c6932e8d21f3cf747be2f3cad2e4
Xbindas1 <166252914+Xbindas1@users.noreply.github.com>
2024-05-07 03:12:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,8 @@
-# Jishu Developer 
+# Xbindas
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Xbindas
+# Backup Channel @Xbindas
+# Developer @Xbindas_Owner
 
 
 
@@ -229,8 +229,8 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
 
 
 
-# Jishu Developer 
+# Xbindas
 # Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @Xbindas
+# Backup Channel @Xbindas
+# Developer @Xbindas_Owner
